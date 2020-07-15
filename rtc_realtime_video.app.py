91c18e01ea29a15e3f95a7e8a84f1dd1e5ed532e
zer0 <osom8979@gmail.com>
2020-07-15 17:40:38 +0900

    Refactoring rtc_realtime_video_server.py script.

--- rtc_realtime_video.app.py
+++ rtc_realtime_video.app.py
@@ -44,140 +44,9 @@ port = 8888
 max_queue_size = DEFAULT_MAX_QUEUE_SIZE
 exit_timeout_seconds = DEFAULT_EXIT_TIMEOUT_SECONDS
 
-app: web.Application
-cors: aiohttp_cors.CorsConfig
 rtc_process: Process
 producer_queue: Queue
-consumer_queue: Queue
 producer_password = ''
-consumer_password = ''
-peer_connections = set()
-empty_img = np.zeros((300, 300, 3), dtype=np.uint8)
-last_frame = VideoFrame.from_ndarray(empty_img, format='bgr24')
-
-
-async def on_exit_process_background():
-    global app
-    await app.shutdown()
-    await app.cleanup()
-    raise web.GracefulExit()
-
-
-async def on_exit_signal(request):
-    data = await request.post()
-    password = data[PASSWORD_PARAM_KEY]
-    global consumer_password
-    if consumer_password == password:
-        asyncio.create_task(on_exit_process_background())
-    return web.Response(content_type='text/html', text='')
-
-
-async def on_index_html(request):
-    return web.Response(content_type='text/html', text=INDEX_HTML_CONTENT)
-
-
-async def on_client_js(request):
-    return web.Response(content_type='application/javascript', text=CLIENT_JS_CONTENT)
-
-
-async def on_offer(request):
-    params = await request.json()
-    offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
-
-    pc = RTCPeerConnection()
-    peer_connections.add(pc)
-
-    @pc.on('iceconnectionstatechange')
-    async def on_iceconnectionstatechange():
-        logging.info('ICE connection state is {}', pc.iceConnectionState)
-        if pc.iceConnectionState == 'failed':
-            await pc.close()
-            peer_connections.discard(pc)
-
-    # open media source
-    # if args.play_from:
-    #     player = MediaPlayer(args.play_from)
-    # else:
-    #     options = {'framerate': '30', 'video_size': '640x480'}
-    #     if platform.system() == 'Darwin':
-    #         player = MediaPlayer('default:none', format='avfoundation', options=options)
-    #     else:
-    #         player = MediaPlayer('/dev/video0', format='v4l2', options=options)
-
-    await pc.setRemoteDescription(offer)
-    for t in pc.getTransceivers():
-        if t.kind == 'video':
-            global consumer_queue
-            pc.addTrack(rtc_realtime_video_server.VideoImageTrack(consumer_queue))
-        elif t.kind == 'audio':
-            pass
-
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-
-    return web.Response(
-        content_type='application/json',
-        text=json.dumps(
-            {'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type}
-        ),
-    )
-
-
-async def on_shutdown(app):
-    # close peer connections
-    coros = [pc.close() for pc in peer_connections]
-    await asyncio.gather(*coros)
-    peer_connections.clear()
-
-
-async def on_cleanup(app):
-    pass
-
-
-def start_app(http_host, http_port, ipc_queue, password, verbose=False, cert_file=None, key_file=None):
-    if verbose:
-        logging.basicConfig(level=logging.DEBUG)
-
-    if cert_file and key_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(cert_file, key_file)
-    else:
-        ssl_context = None
-
-    global consumer_queue
-    consumer_queue = ipc_queue
-
-    global consumer_password
-    consumer_password = password
-
-    logging.info('start_app(host={},port={},password={},verbose={})',
-                 http_host, http_port, password, verbose)
-
-    global app
-    app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-    app.on_cleanup.append(on_cleanup)
-    app.router.add_get(INDEX_HTML_PATH, on_index_html)
-    app.router.add_get(CLIENT_JS_PATH, on_client_js)
-    app.router.add_post(OFFER_PATH, on_offer)
-    app.router.add_post(EXIT_SIGNAL_PATH, on_exit_signal)
-
-    global cors
-    cors = aiohttp_cors.setup(app, defaults={
-        '*': aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers='*',
-            allow_headers='*',
-        )
-    })
-    for route in list(app.router.routes()):
-        cors.add(route)
-
-    web.run_app(app,
-                host=http_host,
-                port=http_port,
-                ssl_context=ssl_context,
-                handle_signals=False)
 
 
 def on_set(key, val):
@@ -215,7 +84,7 @@ def on_init():
     producer_queue = Queue(max_queue_size)
 
     global rtc_process
-    rtc_process = Process(target=start_app, args=(host, port, producer_queue, producer_password,))
+    rtc_process = Process(target=rtc_realtime_video_server.start_app, args=(host, port, producer_queue, producer_password,))
     rtc_process.start()
 
     sys.stderr.write(f'RTC process PID is {rtc_process.pid}.')
--- rtc_realtime_video_server.py
+++ rtc_realtime_video_server.py
@@ -107,5 +107,143 @@ class RealTimeVideoServer:
         pass
 
 
+from aiohttp import web
+import aiohttp_cors
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from multiprocessing import Queue
+import asyncio
+import logging
+import json
+import ssl
+
+app: web.Application
+cors: aiohttp_cors.CorsConfig
+consumer_queue: Queue
+consumer_password = ''
+peer_connections = set()
+
+async def on_exit_process_background():
+    global app
+    await app.shutdown()
+    await app.cleanup()
+    raise web.GracefulExit()
+
+
+async def on_exit_signal(request):
+    data = await request.post()
+    password = data[PASSWORD_PARAM_KEY]
+    global consumer_password
+    if consumer_password == password:
+        asyncio.create_task(on_exit_process_background())
+    return web.Response(content_type='text/html', text='')
+
+
+async def on_index_html(request):
+    return web.Response(content_type='text/html', text=INDEX_HTML_CONTENT)
+
+
+async def on_client_js(request):
+    return web.Response(content_type='application/javascript', text=CLIENT_JS_CONTENT)
+
+
+async def on_offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
+
+    pc = RTCPeerConnection()
+    peer_connections.add(pc)
+
+    @pc.on('iceconnectionstatechange')
+    async def on_iceconnectionstatechange():
+        logging.info('ICE connection state is {}', pc.iceConnectionState)
+        if pc.iceConnectionState == 'failed':
+            await pc.close()
+            peer_connections.discard(pc)
+
+    # open media source
+    # if args.play_from:
+    #     player = MediaPlayer(args.play_from)
+    # else:
+    #     options = {'framerate': '30', 'video_size': '640x480'}
+    #     if platform.system() == 'Darwin':
+    #         player = MediaPlayer('default:none', format='avfoundation', options=options)
+    #     else:
+    #         player = MediaPlayer('/dev/video0', format='v4l2', options=options)
+
+    await pc.setRemoteDescription(offer)
+    for t in pc.getTransceivers():
+        if t.kind == 'video':
+            global consumer_queue
+            pc.addTrack(VideoImageTrack(consumer_queue))
+        elif t.kind == 'audio':
+            pass
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type='application/json',
+        text=json.dumps(
+            {'sdp': pc.localDescription.sdp, 'type': pc.localDescription.type}
+        ),
+    )
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in peer_connections]
+    await asyncio.gather(*coros)
+    peer_connections.clear()
+
+
+async def on_cleanup(app):
+    pass
+
+
+def start_app(http_host, http_port, ipc_queue, password, verbose=False, cert_file=None, key_file=None):
+    if verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if cert_file and key_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(cert_file, key_file)
+    else:
+        ssl_context = None
+
+    global consumer_queue
+    consumer_queue = ipc_queue
+
+    global consumer_password
+    consumer_password = password
+
+    logging.info('start_app(host={},port={},password={},verbose={})',
+                 http_host, http_port, password, verbose)
+
+    global app
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.on_cleanup.append(on_cleanup)
+    app.router.add_get(INDEX_HTML_PATH, on_index_html)
+    app.router.add_get(CLIENT_JS_PATH, on_client_js)
+    app.router.add_post(OFFER_PATH, on_offer)
+    app.router.add_post(EXIT_SIGNAL_PATH, on_exit_signal)
+
+    global cors
+    cors = aiohttp_cors.setup(app, defaults={
+        '*': aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers='*',
+            allow_headers='*',
+        )
+    })
+    for route in list(app.router.routes()):
+        cors.add(route)
+
+    web.run_app(app,
+                host=http_host,
+                port=http_port,
+                ssl_context=ssl_context,
+                handle_signals=False)
+
 if __name__ == '__main__':
     pass
